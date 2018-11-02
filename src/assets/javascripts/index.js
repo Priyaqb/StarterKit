ad884069574c4f82af094630aa5e0326f9bcb3d2
Priya <priyas@qburst.com>
2018-11-02 17:39:17 +0530

    Added files.

--- src/assets/javascripts/index.js
+++ src/assets/javascripts/index.js
@@ -1,29 +1,85 @@
 jQuery(document).ready(function($) {
-  console.log("ffdgfdg");
-  var toggleIcon = $("#toggle-icon"),
-    siteNav = $("#site-nav");
+    var toggleIcon = $("#toggle-icon"),
+        siteNav = $("#site-nav");
 
-  toggleMenu();
-
-  toggleIcon.click(function() {
-    if (siteNav.hasClass("hide-menu")) {
-      siteNav.removeClass("hide-menu").addClass("show-menu");
-    } else {
-      siteNav.removeClass("show-menu").addClass("hide-menu");
-    }
-    $(this).toggleClass('open');
-  });
-
-  $(window).on('resize', function() {
     toggleMenu();
-  });
 
-  function toggleMenu() {
-    if ($(window).width() < 768) {
-      siteNav.addClass("hide-menu");
+    toggleIcon.click(function() {
+        if (siteNav.hasClass("hide-menu")) {
+            siteNav.removeClass("hide-menu").addClass("show-menu");
+        } else {
+            siteNav.removeClass("show-menu").addClass("hide-menu");
+        }
+        $(this).toggleClass('open');
+    });
+
+    $(window).on('resize', function() {
+        toggleMenu();
+    });
+
+    function toggleMenu() {
+        if ($(window).width() < 768) {
+            siteNav.addClass("hide-menu");
+        } else {
+            siteNav.removeClass("hide-menu");
+        }
     }
-    else {
-      siteNav.removeClass("hide-menu");
+
+    document.getElementById("copyHTMLButton").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("copyHTMLTarget"));
+    });
+
+    document.getElementById("copyCSSButton").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("copyCSSTarget"));
+    });
+
+    function copyToClipboard(elem) {
+        // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+
+        // copy the selection
+        var succeed;
+        try {
+            succeed = document.execCommand("copy");
+        } catch (e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        return succeed;
     }
-  }
 });
--- webpack.local.config.js
+++ webpack.local.config.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const autoprefixer = require('autoprefixer');
-var path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+const glob = require("glob");
+
 
 module.exports = [{
         entry: ['./src/index.js'],
@@ -25,19 +28,41 @@ module.exports = [{
                 watch: true
             }),
             new HtmlWebpackPlugin({
+                filename: 'components/sidebar.html',
+                inject: false,
+                template: './src/components/sidebar.html'
+            }),
+            new HtmlWebpackPlugin({
                 filename: 'index.html',
                 inject: false,
                 template: './src/index.html'
             }),
+            new HtmlWebpackPlugin({
+                filename: 'views/header.html',
+                inject: false,
+                template: './src/views/header.html'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'views/footer.html',
+                inject: false,
+                template: './src/views/footer.html'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'views/form.html',
+                inject: false,
+                template: './src/views/form.html'
+            }),
             new CopyWebpackPlugin([
                 {from:'./src/assets/fonts',to:'fonts'},
-                {from:'./src/assets/images',to:'images'} 
-            ]), 
+                {from:'./src/assets/images',to:'images'},
+                {from:'./src/assets/libs',to:'libs'},
+            ])
         ],
         mode: 'development'
     },
     {
         entry: ['./src/assets/stylesheets/index.scss'],
+        //entry: ['./src/assets/stylesheets/index.scss'],
         output: {
             filename: '[name].css',
             path: path.resolve(__dirname, 'dist/styles')
@@ -61,7 +86,52 @@ module.exports = [{
             ]
         },
         plugins: [
-            new ExtractTextPlugin('style.css'),
+            new ExtractTextPlugin('[name].css'),
+        ]
+    },
+    {
+        entry: {
+            header: "./src/assets/stylesheets/components/header/index.scss",
+            footer: "./src/assets/stylesheets/components/footer/index.scss",
+            normalForm: "./src/assets/stylesheets/components/normal-form/index.scss",
+        },
+        //entry: glob.sync('./src/assets/stylesheets/**/*.scss'),
+        output: {
+            filename: '[name].css',
+            path: path.resolve(__dirname, 'dist/styles/components')
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist/styles'),
+            filename: '[name].css',
+            publicPath: '/'
+        },
+        module: {
+            rules: [{
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: ['css-loader',
+                          {
+                            loader: 'postcss-loader',
+                            options: {
+                              plugins: () => [autoprefixer()]
+                            }
+                          },
+                          'sass-loader'    ]
+                        })
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+                },
+                {
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'file-loader'
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('[name].css'),
         ]
     },
     {
